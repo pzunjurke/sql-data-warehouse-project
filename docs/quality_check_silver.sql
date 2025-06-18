7433c9aac4c2a5281f9a2f4b9fbfd5cbfd754a82
pzunjurke <paysh004@gmail.com>
2025-06-18 23:50:34 +0530

    Delete docs directory

--- docs/quality_check_silver.sql
+++
@@ -1,7 +0,0 @@
--- Checck for Nulls or Duplicates in Primary Key
--- Exception: No result
-
-select cst_id,COUNT(*)
-from bronze.crm_cust_info
-group by cst_id
-having count(*) >1 or cst_id is NULL;
