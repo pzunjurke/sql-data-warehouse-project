6d61e7d361f000fde717947c09ede3be7233ec1b
pzunjurke <paysh004@gmail.com>
2025-06-23 09:04:22 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -19,7 +19,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_firstname      NVARCHAR(50),
     cst_lastname       NVARCHAR(50),
     cst_marital_status NVARCHAR(50),
-    cst_gndr           NVARCHAR(50),
+    cst_gender           NVARCHAR(50),
     cst_create_date    DATE,
     dwh_create_date    DATETIME2 DEFAULT GETDATE()
 );
@@ -33,7 +33,7 @@ CREATE TABLE silver.crm_prd_info (
     prd_id          INT,
     cat_id          NVARCHAR(50),
     prd_key         NVARCHAR(50),
-    prd_nm          NVARCHAR(50),
+    prd_name          NVARCHAR(50),
     prd_cost        INT,
     prd_line        NVARCHAR(50),
     prd_start_dt    DATE,
